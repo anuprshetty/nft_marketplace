786e8018c63cfe8889759cf41de6f537fc968048
anuprshetty <anuprshetty@gmail.com>
2023-08-05 19:12:07 +0530

    marketplace_nft_minter mint function

--- contract/contracts/marketplace_nft_minter.sol
+++ contract/contracts/marketplace_nft_minter.sol
@@ -21,4 +21,39 @@ contract MarketplaceNFTMinter is ERC721Enumerable, ERC721URIStorage, Ownable {
             bytes(_symbol).length > 0 ? _symbol : "COL-NUL"
         )
     {}
+
+    function mint(
+        address to,
+        string calldata _NFTMetadataFolderCID,
+        uint256 totalNFTs
+    ) public payable {
+        require(!paused, "minting is paused");
+        require(
+            bytes(_NFTMetadataFolderCID).length > 0,
+            "_NFTMetadataFolderCID should not be empty"
+        );
+        require(totalNFTs > 0, "totalNFTs must be greater than 0");
+        require(
+            msg.value == (cost * totalNFTs),
+            "amount not equal to minting cost"
+        );
+
+        uint256 supply = totalSupply();
+
+        for (uint256 i = 1; i <= totalNFTs; i++) {
+            uint256 tokenId = supply + i;
+            string memory uri = string(
+                abi.encodePacked(
+                    "ipfs://",
+                    _NFTMetadataFolderCID,
+                    "/",
+                    tokenId.toString(),
+                    baseExtension
+                )
+            );
+
+            _safeMint(to, tokenId);
+            _setTokenURI(tokenId, uri);
+        }
+    }
 }
