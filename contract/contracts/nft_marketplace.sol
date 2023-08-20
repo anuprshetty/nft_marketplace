d002690d21cfded3b3b882c8e897e621d73a6329
anuprshetty <anuprshetty@gmail.com>
2023-08-20 08:12:22 +0530

    NFTMarketplace contract

---
+++ contract/contracts/nft_marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTMarketplace is IERC721Receiver, ReentrancyGuard, Ownable {}
