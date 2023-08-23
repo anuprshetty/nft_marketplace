e06e59039a5b244ca7bc64efd4612f10d66fe2f2
anuprshetty <anuprshetty@gmail.com>
2023-08-23 11:45:56 +0530

    updated nftListingFee

--- contract/contracts/nft_marketplace.sol
+++ contract/contracts/nft_marketplace.sol
@@ -8,4 +8,6 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTMarketplace is IERC721Receiver, ReentrancyGuard, Ownable {}
+contract NFTMarketplace is IERC721Receiver, ReentrancyGuard, Ownable {
+    uint256 public nftListingFee = 0.0025 ether;
+}
