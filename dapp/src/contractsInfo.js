4bdbbb8f4d47fce16ee9047ce592b3a5c878f172
anuprshetty <anuprshetty@gmail.com>
2023-10-04 15:21:28 +0530

    contracts_info

---
+++ dapp/src/contractsInfo.js
@@ -0,0 +1,13 @@
+import Token from "./contracts_info/Token.json";
+import NFTMinter from "./contracts_info/NFTMinter.json";
+import MarketplaceNFTMinter from "./contracts_info/MarketplaceNFTMinter.json";
+import NFTMarketplace from "./contracts_info/NFTMarketplace.json";
+
+const contractsInfo = {
+  Token: Token,
+  NFTMinter: NFTMinter,
+  MarketplaceNFTMinter: MarketplaceNFTMinter,
+  NFTMarketplace: NFTMarketplace,
+};
+
+export default contractsInfo;
